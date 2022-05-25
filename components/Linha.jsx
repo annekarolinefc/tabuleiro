9badff69c206f6c94ebb4b2338811f5c185386e1
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-25 10:54:22 -0300

    Editando as propriedades da linha

--- components/Linha.jsx
+++ components/Linha.jsx
@@ -1,17 +1,18 @@
 import styles from '../styles/Linha.module.css'
 import Subdivisao from "./Subdivisao";
+//import styles from "../styles/Linha.module.css"
 
 export default function linha(props){
     return(
         <div className={styles.linha}>
-            <Subdivisao />
-            <Subdivisao preta />
-            <Subdivisao />
-            <Subdivisao preta />
-            <Subdivisao />
-            <Subdivisao preta />
-            <Subdivisao />
-            <Subdivisao preta />
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta}/>
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta}/>
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta}/>
+            <Subdivisao preta={props.preta} />
+            <Subdivisao preta={!props.preta}/>
         </div>
     )
 }
