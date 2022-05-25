707bdf7846a6d8bdda422910a135eeb5db908c4c
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-05-25 10:33:21 -0300

    Criando a linha do tabuleiro

---
+++ components/Linha.jsx
@@ -0,0 +1,17 @@
+import styles from '../styles/Linha.module.css'
+import Subdivisao from "./Subdivisao";
+
+export default function linha(props){
+    return(
+        <div className={styles.linha}>
+            <Subdivisao />
+            <Subdivisao preta />
+            <Subdivisao />
+            <Subdivisao preta />
+            <Subdivisao />
+            <Subdivisao preta />
+            <Subdivisao />
+            <Subdivisao preta />
+        </div>
+    )
+}
